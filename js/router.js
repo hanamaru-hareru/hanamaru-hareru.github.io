fd7ed22ac677ec07d60613dc15929d8a52af111f
Saki Tojo <haolei.ye@anu.edu.au>
2021-12-10 01:46:44 +0800

    Add database loading dependencies.

--- js/router.js
+++ js/router.js
@@ -12,17 +12,22 @@ const router_map = {
 }
 
 const database_loader = [
-    ['forecast.json', forecast_load_data],
-    ['single.json', single_load_data],
-    ['button-voices.json', voice_load_data],
-    ['slides.json', function(raw_data) { slides.records = raw_data; }],
-    ['song-details.json', sl_song_info_load_data],
-    ['song-list.json', sl_song_list_load_data],
-    ['videos.json', stream_load_data],
+    [
+        ['forecast.json', forecast_load_data],
+        ['single.json', single_load_data],
+        ['slides.json', function(raw_data) { slides.records = raw_data; }],
+        ['song-details.json', sl_song_info_load_data],
+    ],
+    [
+        ['button-voices.json', voice_load_data],
+        ['song-list.json', sl_song_list_load_data],
+    ],
+    [
+        ['videos.json', stream_load_data],
+    ],
 ];
 
 let database_load_start = undefined;
-let database_load_counter = 0;
 
 function router_handle_url() {
     // Parse the url.
@@ -54,11 +59,24 @@ function app_ui_init() {
     window.addEventListener('hashchange', router_handle_url, false);
 }
 
-function app_database_init() {
+function app_database_init(level) {
+    //Check whether all the level reach are loaded.
+    if(level >= database_loader.length) {
+        const database_load_end = new Date().getTime();
+        console.log('Database cached: ' + (database_load_end - database_load_start) + 'ms');
+        //Enabled app cache.
+        app_fetch_cache_enabled = true;
+        //Load the user interface.
+        app_ui_init();
+        return;
+    }
+    //First load the level information.
+    const database_level_info = database_loader[level];
+    let database_load_counter = 0;
     //Check id validation.
-    for(let current_id=0;  current_id<database_loader.length; ++current_id) {
+    for(let loader_id=0; loader_id<database_level_info.length; ++loader_id) {
         //Extract the database file info.
-        const load_info = database_loader[current_id];
+        const load_info = database_level_info[loader_id];
         const load_url = '/database/' + load_info[0], assign_callback = load_info[1];
         //Fetch the json data.
         app_fetch(load_url, function (json_raw_data) {
@@ -66,13 +84,9 @@ function app_database_init() {
             assign_callback(JSON.parse(json_raw_data));
             // Increase the database counter.
             database_load_counter += 1;
-            if(database_load_counter === database_loader.length) {
-                const database_load_end = new Date().getTime();
-                console.log('Database cached: ' + (database_load_end - database_load_start) + 'ms');
-                //Enabled app cache.
-                app_fetch_cache_enabled = true;
-                //Load the user interface.
-                app_ui_init();
+            if(database_load_counter === database_level_info.length) {
+                //Load the next level.
+                app_database_init(level + 1);
             }
         });
     }
@@ -97,5 +111,5 @@ function app_init() {
         '      .:.:...::\n', 'color: #db9854');
     // Load the database to local.
     database_load_start = new Date().getTime();
-    app_database_init();
+    app_database_init(0);
 }
