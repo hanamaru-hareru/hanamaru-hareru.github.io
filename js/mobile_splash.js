80f1e578da6b7a6b52ac74c5829ba14bbe30a5e7
Harinlen <tomguts@126.com>
2021-12-13 01:56:48 +0800

    Add localstorage splash

--- js/mobile_splash.js
+++ js/mobile_splash.js
@@ -1,6 +1,7 @@
 // Check whether the website is running on mobile.
 let show_splash = true;
-const splash_disable_key = 'no-splash', splash_cache_key = 'splash_img';
+let splash_blob = undefined;
+const splash_disable_key = 'no-splash', splash_cache_key = 'splash_img', splash_res_key = 'splash_res';
 const resolution_map = {
     "640x1136": "Default-568h@2x.png",
     "750x1334": "Default-667h@2x.png",
@@ -23,10 +24,19 @@ function splash_hide() {
     }
 }
 
+function set_splash_and_start(splash_res) {
+    let splash_div = document.getElementById('splash-screen');
+    splash_div.style.backgroundImage = 'url('+splash_res+')';
+    // console.log(splash_div.style.backgroundImage)
+    splash_div.style.backgroundRepeat = 'no-repeat';
+    splash_div.style.backgroundSize = 'cover';
+    splash_div.style.backgroundPosition = 'center center';
+    app_init();
+}
+
 function splash_init() {
     const clientInfo = navigator.userAgent;
     let handle_splash = clientInfo.indexOf('Android') > -1 || clientInfo.indexOf('iPhone') > -1 || clientInfo.indexOf('iPad') > -1 || clientInfo.indexOf('iPod') > -1;
-    let splash_div = document.getElementById('splash-screen');
     //Check temporary disable flag exist.
     if(handle_splash && app_load_conf(splash_disable_key, false)) {
         //Clear the remove key.
@@ -37,13 +47,12 @@ function splash_init() {
     //If not on mobile or we have to skip it, hide the splash screen.
     if(!handle_splash) {
         splash_hide();
+        //Initial the application directly.
+        app_init();
         return;
     }
     //Check whether we already has a cached splash image.
-    let cached_splash = app_load_conf(splash_cache_key, undefined);
-    // if(cached_splash) {
-    //     console.log(cached_splash);
-    // }
+    let cached_splash = app_load_conf(splash_cache_key, undefined), cached_res = app_load_conf(splash_res_key, '');
     //Now get the resolution, and select the splash screen.
     const screen_width = window.screen.width, screen_height = window.screen.height;
     const res_1x = screen_width+'x'+screen_height, res_2x = (screen_width*2)+'x'+(screen_height*2);
@@ -57,10 +66,22 @@ function splash_init() {
     } else if (res_1x in resolution_map) {
         splash_url = resolution_map[res_1x];
     }
-    splash_div.style.backgroundImage = 'url("' + splash_url + '")';
-    splash_div.style.backgroundRepeat = 'no-repeat';
-    splash_div.style.backgroundSize = 'cover';
-    splash_div.style.backgroundPosition = 'center center';
+    //Check resource has and resolution matches.
+    if(cached_splash && cached_res === splash_url) {
+        set_splash_and_start(cached_splash);
+    } else {
+        fetch(splash_url).then(function(res) {
+            res.blob().then(function(splash_data) {
+                const reader = new FileReader();
+                reader.onloadend = function() {
+                    //Save the image data to local storage.
+                    app_set_conf(splash_res_key, splash_url);
+                    app_set_conf(splash_cache_key, reader.result);
+                    // Fetch the cached splash data.
+                    set_splash_and_start(app_load_conf(splash_cache_key, undefined));
+                }
+                reader.readAsDataURL(splash_data);
+            });
+        });
+    }
 }
-
-splash_init();
