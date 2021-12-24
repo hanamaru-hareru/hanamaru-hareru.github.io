baa00c2f29be654a7e1ff8bee8c27018811132c7
Harinlen <tomguts@126.com>
2021-12-25 03:02:50 +0800

    Update the theme duration.

--- js/theme-loader.js
+++ js/theme-loader.js
@@ -3,6 +3,7 @@ const theme_settings = {
     '11-1': 'theme-halloween.css',
     '12-24': 'theme-christmas.css',
     '12-25': 'theme-christmas.css',
+    '12-26': 'theme-christmas.css',
 }
 const theme_date = new Date();
 const date_key = (theme_date.getMonth()+1) + '-' + theme_date.getDate();
