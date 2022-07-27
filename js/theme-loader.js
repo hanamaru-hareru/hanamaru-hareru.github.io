180ef19b90409ff870e1b76a80ed46d0b5fc15be
Saki Tojo <haolei.ye@anu.edu.au>
2022-07-28 07:00:56 +1000

    Fixed the theme bug

--- js/theme-loader.js
+++ js/theme-loader.js
@@ -11,6 +11,5 @@ const date_key = (theme_date.getMonth()+1) + '-' + theme_date.getDate();
 if(date_key in theme_settings) {
     document.write('<link href="css/'+theme_settings[date_key]+'" rel="stylesheet">');
 } else {
-    //document.write('<link href="css/theme-normal.css" rel="stylesheet">');
-    document.write('<link href="css/theme-christmas.css" rel="stylesheet">');
+    document.write('<link href="css/theme-normal.css" rel="stylesheet">');
 }
